9b9fac2f7f45f6a74effd000df5bd4dd389469d2
nicoleedwards <nicole.edwards722@gmail.com>
2015-04-09 23:40:35 +0000

    finished

--- fitter.py
+++ fitter.py
@@ -1,4 +1,7 @@
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 #  ------------------------------------------------------
@@ -6,100 +9,92 @@ import matplotlib.pyplot as plt
 #  it as a png image ...
 #
 def plotData(x,y,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.title('Raw Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.title('Raw Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  PLOTFIT: plots line AND data and saves it as a png
-#
+#plotData(x,h0,badFit.png)
 #
 def plotFit(x,y,z,name):
-	plt.clf()
-	plt.plot(x,y,'k.')
-	plt.plot(x,z,'k-')
-	plt.title('Fit to Data')
-	plt.savefig(name)
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.plot(x,z,'k-')
+    plt.title('Fit to Data')
+    plt.savefig(name)
 
 #  ------------------------------------------------------
 #  GRABDATA
 #    Pulls data from file and stores it in x,y
 #
 def grabData():
-	f = open('linFit.dat','r')
-		
-	dataString = f.read()
-	f.close()
-	dataLines = dataString.split('\n');
-	N = len(dataLines)-1
-	x = []
-	y = []
+    dat=np.genfromtxt('linFit.txt')
+    x=dat[:,0]
+    y=dat[:,1]
 
-	for i in range(N):
-		line = dataLines[i].split('\t')
-		x.append(float( line[0] ))
-		y.append(float( line[1] ))
+    return (x,y)
 
-	return (x,y)
-	
 
 
 #  ------------------------------------------------------
-#  F
-#    Given data list x, returns a list y = mx+b 
-#
-def f(x,m,b):
-	return 0					# FIXME!
+# Step 3: Given list of x return list of h0*x
+def hubble(x,h0):
+    return [h0*i for i in x]
 
-#
-#   SSR
-# 
+
 #     Given data lists x and y, this function
-#     finds how well a line, f(x) = mx+b fits
+#     finds how well a line, hubble(x) = h0*x fits
 #     by calculating the sum of the square of
 #     the residuals
 #
-def SSR(x,y,m,b):
-	return 0 					# FIXME!
 
-#
-#  STEP 3 ---- Get data, plot it
-#
+#Step 5:
+def SSR(x,y,h0):
+    return 0
 
 
 #
-#  STEP 5 ---- Plot example function
+#  Step 2: Get data, plot it
 #
 
-#
-#  STEP 7 ---- Find SSR for poor fit from example function
-#
+(x,y) =grabData()
+print x
+plotData(x,y,'rawData.png')
+
+#   Step 4: Plot example function
+
+h0=10
+z=hubble(x,h0)
+print z
+plotFit(x,y,z,'badFit.png')
 
 #
-#  STEP 8 ---- Make lists for possible slopes and
-#              intercepts, then print them
+#  Step 6: Find SSR for poor fit from example function
 #
 
-slopes = []
-for i in range(-50,50):
-	slopes.append( i/10.0 )
 
 #
-#  STEP 9 ---- Find slope and intercept that minimize
+#   list for possible slopes. 
+#
+
+h0s=np.arange(0.,100.,0.1)
+
+#
+#  Step 7: Find slope and intercept that minimize
 #              the RSS
 #
-bestSlope = 0
-bestInter = 0
-minSSR = 10000
+best_h0 = 0.
+min_ssr = np.inf
 
 #
-#  STEP 10 ---- Plot Result
+#   Step 8: Plot Result
 #
 #
 
 
 #
-#  STEP 11 ---- Plot Residuals
+#   Step 9: Plot Residuals
 #
 #
--- sandbox.py
+++ sandbox.py
@@ -45,3 +45,106 @@ for z in range(-20,20):
                 least = f(z)
                 leastx = z
 print leastx
+
+
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#  ------------------------------------------------------
+#  PLOTDATA: plots a line and saves
+#  it as a png image ...
+#
+def plotData(x,y,name): 
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.title('Raw Data')
+    plt.savefig(name)
+
+#  ------------------------------------------------------
+#  PLOTFIT: plots line AND data and saves it as a png
+#
+#
+def plotFit(x,y,z,name):
+    plt.clf()
+    plt.plot(x,y,'k.')
+    plt.plot(x,z,'k-')
+    plt.title('Fit to Data')
+    plt.savefig(name)
+
+#  ------------------------------------------------------
+#  GRABDATA
+#    Pulls data from file and stores it in x,y
+#
+def grabData():
+    dat=np.genfromtxt('linFit.txt')
+    x=dat[:,0]
+    y=dat[:,1]
+
+    return (x,y)
+
+
+
+#  ------------------------------------------------------
+# Step 3: Given list of x return list of h0*x
+def hubble(x,h0):
+    return 0.
+
+
+#     Given data lists x and y, this function
+#     finds how well a line, hubble(x) = h0*x fits
+#     by calculating the sum of the square of
+#     the residuals
+#
+
+#Step 5:
+def SSR(x,y,h0):
+    return 0
+
+
+#
+#  Step 2: Get data, plot it
+#
+
+x, y=grabData()
+
+#   Step 4: Plot example function
+#
+
+
+#
+#  Step 6: Find SSR for poor fit from example function
+#
+
+
+#
+#   list for possible slopes. 
+#
+
+h0s=np.arange(0.,100.,0.1)
+
+#
+#  Step 7: Find slope and intercept that minimize
+#              the RSS
+#
+best_h0 = 0.
+min_ssr = np.inf
+
+#
+#   Step 8: Plot Result
+#
+#
+
+
+#
+#   Step 9: Plot Residuals
+#
+#
